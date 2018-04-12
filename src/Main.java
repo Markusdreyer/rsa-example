5342fe99a2540c0d719108b98ecb13dc032dc2c1
Markus Dreyer <markus.a.dreyer@gmail.com>
2018-04-12 11:49:34 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    private int letterNumber = 1;
+
+
+    public String lookupTable() {
+        String letter = "";
+        switch (letterNumber) {
+            case 1:
+                letter = "A";
+                break;
+            case 2:
+            
+        }
+
+        return letter;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.pow(2,5) % 14);
+
+    }
+
+}
