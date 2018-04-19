867e1f86b9ff7bc4beb7aa6d9ffa0cdfe356d131
Markus Dreyer <markus.a.dreyer@gmail.com>
2018-04-19 12:41:51 +0200

    started working on euliers totient

---
+++ src/CalculatePhiTest.java
@@ -0,0 +1,13 @@
+public class CalculatePhiTest {
+    public static void main(String[] args) {
+        int p = 2;
+        int q = 10;
+        int n = p * q;
+        int phiOfN = (p - 1) * (q - 1);
+        System.out.println("Phi of N: " + phiOfN + "\nN: " + n);
+    }
+
+    /*
+    TODO: Fix Euliers Totient
+     */
+}
